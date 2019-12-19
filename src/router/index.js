1f30890c5f25d1810ce9db0a247cd2c9d41eceb3
unknown <skopin.n@gmail.com>
2019-12-20 00:17:17 +0700

    eslint setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import App from "../App";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from '../App.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: App
-  }
+    path: '/',
+    name: 'home',
+    component: App,
+  },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
