f856abe501085245f60c6eafeafbc0f03cb00e97
unknown <skopin.n@gmail.com>
2019-12-23 15:04:58 +0700

    login page with validation

--- src/components/forms/LoginForm.vue
+++ src/components/forms/LoginForm.vue
@@ -1,56 +1,71 @@
 <template>
   <div>
-    <b-form @submit="onSubmit">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="email"
-        description="We'll never share your email with anyone else."
-      >
+    <b-form @submit="onSubmit" novalidate>
+      <b-form-group id="input-group-1" label="Email address:" label-for="email">
         <b-form-input
           id="email"
           v-model="userData.email"
           type="email"
-          required
-          placeholder="Enter email"
+          placeholder="example@example.com"
         ></b-form-input>
+        <InlineError :text="errors.email" />
       </b-form-group>
-
       <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
         <b-form-input
           id="password"
           v-model="userData.password"
           type="password"
-          required
           placeholder="Password"
         ></b-form-input>
+        <InlineError :text="errors.password" />
       </b-form-group>
 
       <b-form-group id="input-group-3">
         <b-form-checkbox v-model="stayLoggedIn">Stay logged in</b-form-checkbox>
       </b-form-group>
-
       <b-button type="submit" variant="primary">Submit</b-button>
     </b-form>
   </div>
 </template>
 <script>
+import Validator from 'validator';
+import InlineError from '../messages/InlineError.vue';
+
 export default {
+  name: 'LoginForm',
+
   data() {
     return {
       userData: {
         email: '',
-        password: ''
+        password: '',
       },
       loading: false,
-      errors: {},
-      stayLoggedIn: false
+      errors: {
+        email: '',
+        password: '',
+      },
+      stayLoggedIn: false,
     };
   },
+  components: {
+    InlineError,
+  },
   methods: {
-    onSubmit() {}
-  }
+    onSubmit(e) {
+      e.preventDefault();
+      const errors = this.validate(this.userData);
+      this.errors = errors;
+    },
+    validate(data) {
+      const errors = {};
+      if (!Validator.isEmail(data.email)) errors.email = 'Invalid email';
+      if (!data.password) errors.password = "Can't be blank";
+      console.log(this.errors);
+
+      return errors;
+    },
+  },
 };
 </script>
-<style>
-</style>
+<style></style>
---
+++ src/components/messages/InlineError.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="inline-error">{{ text }}</span>
+</template>
+
+<script>
+export default {
+  name: 'InlineError',
+  props: ['text'],
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.inline-error {
+  display: block;
+  color: #ae5856;
+  margin-bottom: 10px;
+}
+</style>
